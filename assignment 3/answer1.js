5079ad459fcdcdfd15c742be003781bc1a22a2db
Vish2810 <66877910+Vish2810@users.noreply.github.com>
2020-07-21 10:41:53 +0530

    Day 3 Assignments

---
+++ assignment 3/answer1.js
@@ -0,0 +1,14 @@
+console.log('Even And Odd');
+let no=parseInt(prompt("Enter the Number"))
+
+
+function evOd()
+{
+    if(no%2==0)
+    console.log(`The Number Entered is ${no} and the number is even`);
+    else
+    console.log(`The Number Entered is ${no} and the number is odd`);
+
+}
+evOd(no)
+
---
+++ assignment 3/answer2.js
@@ -0,0 +1,12 @@
+console.log('OS And Version');
+let Os=prompt("Enter the Os");
+let ver=prompt("Enter the Version");
+
+
+function osVer()
+{
+    console.log(`The OS name is ${Os} and version is ${ver}`);
+   
+
+}
+osVer(Os,ver);
